5558d712323f69c39bd6fbc3585afd41f1e2a5ea
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2025-07-23 02:09:30 -0300

    004 [Feat]: Validates user e order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates :pickup_address, :delivery_address, :requested_at, :estimated_price, presence: { message: "Não pode estar vazio"}
 end
