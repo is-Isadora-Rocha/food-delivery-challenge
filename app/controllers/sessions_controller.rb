09b1088fc5d49135b7f6b489608808026216a71a
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2025-07-25 02:41:32 -0300

    007 [Refactor]: Melhoria nas mensagens de erro do frontend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,38 @@
 class SessionsController < ApplicationController
   def new
+    @user_login = UserLogin.new
   end
 
   def create
+    @user_login = UserLogin.new(login_params)
 
-    user = User.find_or_initialize_by(email: params[:email])
-    user.name = params[:name]
+    if @user_login.valid?
+      user = User.find_or_initialize_by(email: @user_login.email)
+      user.name = @user_login.name
 
-    if user.save
+      if user.save
       session[:user_id] = user.id
       redirect_to orders_path
+      else
+        flash[:alert] = I18n.t("errors.user.not_found")
+        render :new, status: :unprocessable_entity
+      end
     else
-      flash[:alert] = "Você precisa preencher nome e email."
-      redirect_to new_session_path
+      flash[:alert] = I18n.t("login.errors.blank_fields")
+      render :new, status: :unprocessable_entity
     end
-
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_session_path
+    # redirect_to new_session_path
+    render :new, status: :unprocessable_entity
+  end
+
+
+  private
+
+  def login_params
+    params.require(:user_login).permit(:name, :email)
   end
 end
