0d8f20a21741a5bdffd6884139ab636b862a76de
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2025-07-25 12:22:46 -0300

    011 [Refactor] Routes organizado

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
+
+  # ----------------- API ----------------- #
   namespace :api do
-  resources :users, only: [:create]
-  resources :orders, only: [:create]
-  get 'orders/user/:user_id', to: 'orders#by_user'
-end
+    get 'orders/user/:id', to: 'orders#by_user'
+    resources :orders, only: [:index, :create, :show]
+  end
 
-
-  root "sessions#new"
-  resources :orders, only: [:index, :new, :create]
+  # ------------- Web Interface -------------#
+  # Sessões (Login Fake)
   resource :session, only: [:new, :create, :destroy]
+  root "sessions#new"
+
+  # Pedidos (Orders)
+  # Busca de pedidos por ID de usuário formulário de busca de pedidos
+  resources :orders, only: [:index, :new, :create] do
+    collection do
+      get :by_user_form
+      post :by_user
+    end
+  end
 end
