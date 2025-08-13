0a9234158b12018ad582a1cef92ea029f6db9237
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2025-08-13 20:33:04 -0300

    gemfile devise

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,14 @@ class Order < ApplicationRecord
             :requested_at, :estimated_price, presence: true
 
   validates :estimated_price, numericality: { greater_than: 0 }
+
+  validate :delivery_address_different_from_pickup
+
+  private
+
+  def delivery_address_different_from_pickup
+    if pickup_address == delivery_address
+      errors.add(:delivery_address, "deve ser diferente do endereço de coleta")
+    end
+  end
 end
